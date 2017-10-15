fba13d716041e982cad8b66cd02212fbb70e3797
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-15 23:26:02 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return  array.join
   elsif array.length == 2
-    return array.insert(-2, "and")
+    return array.join.insert(-2, "and")
 
   end
 end
