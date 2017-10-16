29ea8b3af0f225a026cd6f558a76073567b15540
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-16 02:48:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return  array.join
+  elsif array.length == 2
+    return array.insert(-2, " and ").join
+  else array.length >= 3
+    first = array.slice(0..-2)
+    last = array.slice(-1)
+    return first.join(", ") + ", and " + last
+  end
 end
