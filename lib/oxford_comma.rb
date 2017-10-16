e5d44edf12edc1fa464238a030dc9f93790ac4c4
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-16 00:18:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.insert(-2, " and ").join
   elsif array.length == 3
-    return array.join(", ")
+    return array.join(", ").insert(" and ")
 
   end
 end
